4cf8d0809a42f542887ae3a3e334685c3e1ae920
Alexandre Cesar <acmorcego@gmail.com>
2018-05-11 13:48:30 -0300

    configure tucker

---
+++ src/sort.lisp
@@ -0,0 +1,8 @@
+(include <stdio.h>)
+(include "mem.h")
+
+(function a_menor_que_b ((char* a) (char* b) (int length)) -> int
+	(return 0))
+
+(function external_sort ((const char* input_file) (const char* output_file) (unsigned int memory)) -> void
+	(return))
