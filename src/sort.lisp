86920e8dabad2a276ff2571ffd259b39d7347f25
Alexandre Cesar <acmorcego@gmail.com>
2018-05-13 20:28:55 -0300

    implement file duplication

--- src/sort.lisp
+++ src/sort.lisp
@@ -1,8 +1,37 @@
 (include <stdio.h>)
 (include "mem.h")
 
-(function a_menor_que_b ((char* a) (char* b) (int length)) -> int
-	(return 0))
+;; Copy the contents of a file to another file
+;; @param source Source file
+;; @param destination Destination file
+;; @param chunkSize Size of each chunk to be copied, in kilobytes
+(function fcopy ((FILE* source)
+								 (FILE* destination)
+								 (unsigned int chunkSize)) -> void
+	(rewind source)
+	(rewind destination)
 
-(function external_sort ((const char* input_file) (const char* output_file) (unsigned int memory)) -> void
-	(return))
+	(decl ((size_t size = (* 1024 chunkSize))
+				 (char* buffer = (mathias_malloc size))
+				 (int bytesRead)))
+	(while (> (set bytesRead (fread buffer 1 size source)) 0)
+		(fwrite buffer 1 bytesRead destination))
+
+	(mathias_free buffer)
+	(rewind source)
+	(rewind destination))
+
+;; Sort a file and output the content to another file, using a maximum
+;; of memory indicated by the user
+;; @param inputPath Path to the input file
+;; @param outputPath Path to the output file
+;; @param memory Maximum memory to be allocated, in kilobytes
+(function external_sort ((const char* inputPath)
+												 (const char* outputPath)
+												 (unsigned int memory)) -> void
+	(decl ((FILE* input = (fopen inputPath "r"))
+				 (FILE* output = (fopen outputPath "rw"))))
+	(fcopy input output memory)
+
+	(fclose input)
+	(fclose output))
